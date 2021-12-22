0e4688c116844da338e7315e2265480641075ace
jiang222kt <jiang222kt@g.ecc.u-tokyo.ac.jp>
2021-12-22 21:42:59 +0900

    finished my_bouncing2

--- bouncing.c
+++ bouncing.c
@@ -33,7 +33,6 @@ int main(int argc, char **argv)
     
     // 表示の座標系は width/2, height/2 のピクセル位置が原点となるようにする
     plot_objects(objects, objnum, t, cond);
-    
     usleep(200 * 1000); // 200 x 1000us = 200 ms ずつ停止
     printf("\e[%dA", cond.height+3);// 壁とパラメータ表示分で3行
   }
